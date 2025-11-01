217fe0a0414720c18c4064542d7646aaf7ccd314
Gaspard Anna Bucher <g.a.bucher@midasum.com>
2025-11-01 11:54:08 +0100

    Add support for mermaid and katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,30 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import remarkMath from 'remark-math';
+import remarkMermaid from 'remark-mermaid';
+import rehypeKatex from 'rehype-katex';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeMermaid from 'rehype-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [
+    mdx({
+      syntaxHighlight: false,
+      remarkPlugins: [remarkMath, remarkMermaid],
+      rehypePlugins: [
+        rehypeKatex,
+        [rehypeMermaid, { strategy: 'img-svg', dark: true }],
+        [
+          rehypePrettyCode,
+          {
+            theme: 'github-dark',
+            keepBackground: false,
+          },
+        ],
+      ],
+    }),
+    tailwind(),
+  ],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -6,10 +6,24 @@ const blog = defineCollection({
     title: z.string(),
     date: z.coerce.date(),
     category: z.string(),
+    image: z.string().optional(),
+  }),
+});
+
+const projects = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    tags: z.array(z.string()),
+    website: z.string().optional(),
+    github: z.string().optional(),
+    image: z.string().optional(),
   }),
 });
 
 export const collections = {
   blog,
+  projects,
 };
 
