b0750ad080cff14d7e408242caa78f39cc53de85
Gaspard Anna Bucher <g.a.bucher@midasum.com>
2025-11-01 09:42:25 +0100

    Initial draft

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
